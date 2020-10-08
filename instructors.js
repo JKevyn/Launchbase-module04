2f51f432a0ba27094a6812ceda04fa9f777f9cd4
Kevyn Gonçalges <kevyn.gon.alu.lmb@gmail.com>
2020-10-08 18:11:04 -0300

    v02

--- instructors.js
+++ instructors.js
@@ -1,5 +1,26 @@
 const fs = require('fs')
 const data = require('./data.json')
+const { age, date } = require('./utils')
+
+//show
+exports.show = function(req, res){
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor) {
+        return instructor.id == id
+    })
+
+    if(!foundInstructor) return res.send('Instructor not found')
+
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(','),
+        created_at: new Intl.DateTimeFormat('pt-BR').format(foundInstructor.created_at)
+    }
+    return res.render('instructors/show', { instructor })
+}
+
 // create
 exports.post = function(req, res){
 
@@ -10,11 +31,22 @@ exports.post = function(req, res){
             return res.send('Please, fill all fields!')
         }
     }
+    
+    let { avatar_url, birth, name, services, gender } = req.body
 
-    req.body.birth = Date.parse(req.body.birth)
-    req.body.created_at = Date.now()
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length + 1)
 
-    data.instructors.push(req.body)
+    data.instructors.push({ 
+        id, 
+        avatar_url, 
+        name, 
+        birth, 
+        gender,
+        services, 
+        created_at 
+    })
 
     fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
         if (err) return res.send('Write file error')
@@ -24,7 +56,23 @@ exports.post = function(req, res){
 
     // return res.send(req.body)
 }
-// update
+
+//edit
+exports.edit = function(req, res) {
+
+    const { id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor) {
+        return instructor.id == id
+    })
+
+    if(!foundInstructor) return res.send('Instructor not found')
+
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
 
 
-// delete
+    return res.render('instructors/edit', {instructor: foundInstructor})
+}
